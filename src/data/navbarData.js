43e4b36baa0d74cc41cefa93d347b86945847194
nFinityV2 <nFinitySoh@hotmail.com>
2023-11-14 17:51:50 +1100

    Added FontAwesome Icons, started initial dropdown menu designs

---
+++ src/data/navbarData.js
@@ -0,0 +1,32 @@
+export const navbarData = [
+    {
+        id: 1,
+        title: 'How it works',
+        submenu: []
+    },
+    {
+        id: 2,
+        title: 'Workflows',
+        submenu: ["Audio Console", "Photo Console", "Video Console", "Mini Console"]
+    },
+    {
+        id: 3,
+        title: 'Download',
+        submenu: []
+    },
+    {
+        id: 4,
+        title: 'Blog',
+        submenu: []
+    },
+    {
+        id: 5,
+        title: 'Support',
+        submenu: []
+    },
+    {
+        id: 6,
+        title: 'Shop',
+        submenu: ["USD","CAD","EUR", "AUD", "GBP", "KRW", "JPY"]
+    },
+];
